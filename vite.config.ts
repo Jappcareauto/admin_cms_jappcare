4b00e0ca9b5ea98b8d93e910d8fdf53ff2af461d
SimoOdilon27 <odilonsimo27@gmail.com>
2025-02-03 12:47:48 -0700

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    host: 'ec2-3-138-123-2.us-east-2.compute.amazonaws.com',
+    strictPort: true,
+    port: 5173 // You can change this to your desired port
+  }
 })
