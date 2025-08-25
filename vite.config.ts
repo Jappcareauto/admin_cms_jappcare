7bf4d66252aae72dd573e6b7990d1cb6c6487fe1
AKOB TAH BANJONG <86370751+Liquidator75@users.noreply.github.com>
2025-08-25 20:40:03 +0100

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   // server: {
-//   //   host: 'ec2-3-138-123-2.us-east-2.compute.amazonaws.com',
-//   //   strictPort: true,
-//   //   port: 5174 // You can change this to your desired port
-//   // }
-//   global: 'window',
-// })
-
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or your framework
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window', // This is the correct way
+  server: {
+    host: true, // allow external access
+    port: 5173, // match your container port
+    strictPort: true,
+    hmr: {
+      host: 'admin.jappcare.com',
+    },
+    allowedHosts: ['admin.jappcare.com'], // <-- ADD THIS LINE
   },
 })
