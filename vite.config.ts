58828abf32c9ac160bc9ce6fdf320c1e9cb2bc34
AKOB TAH BANJONG <86370751+Liquidator75@users.noreply.github.com>
2025-08-26 03:16:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',       // allow connections from any IP
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      host: 'admin.jappcare.com',  // allow HMR over your domain
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
   },
-})
+  define: {
+    global: "window",
+  },
+  server: {
+    allowedHosts: ["servicecenter.jappcare.com", "admin.jappcare.com"],
+    proxy: {
+      "/ws": {
+        target: "https://bpi.jappcare.com",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});
