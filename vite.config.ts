57f2b8a73bc061d52f9944a9f41f38979bbea9e9
AKOB TAH BANJONG <86370751+Liquidator75@users.noreply.github.com>
2025-08-26 03:28:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,24 @@
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // server: {
+//   //   host: 'ec2-3-138-123-2.us-east-2.compute.amazonaws.com',
+//   //   strictPort: true,
+//   //   port: 5174 // You can change this to your desired port
+//   // }
+//   global: 'window',
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
   define: {
-    global: "window",
+    global: 'window', // This is the correct way
   },
-  server: {
-    allowedHosts: ["servicecenter.jappcare.com", "admin.jappcare.com"],
-    proxy: {
-      "/ws": {
-        target: "https://bpi.jappcare.com",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
-});
+})
